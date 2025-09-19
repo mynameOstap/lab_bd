6c1c89d3ebbafb66b22fbd9814145a0d037334a8
Ostap <ostduf2@gmail.com>
2025-09-19 19:19:45 +0300

    f

--- my_project/auth/route/orders/CarBlueprint.py
+++ my_project/auth/route/orders/CarBlueprint.py
@@ -3,17 +3,46 @@ from flask import Blueprint, jsonify, Response, request, make_response
 from my_project.auth.controller import car_controller
 from my_project.auth.domain.orders.Car import Car
 
-
 car_bp = Blueprint('car', __name__, url_prefix='/car')
 
 @car_bp.get('')
 def get_all_cars() -> Response:
+    """
+    Get all cars
+    ---
+    tags:
+      - Cars
+    responses:
+      200:
+        description: List of cars
+        examples:
+          application/json: [
+            {"id": 1, "brand": "BMW", "model": "X5"},
+            {"id": 2, "brand": "Audi", "model": "A6"}
+          ]
+    """
     cars = car_controller.find_all()
     cars_dto = [car.put_into_dto() for car in cars]
     return make_response(jsonify(cars_dto), HTTPStatus.OK)
 
 @car_bp.post('')
 def create_car() -> Response:
+    """
+    Create a new car
+    ---
+    tags:
+      - Cars
+    requestBody:
+      required: true
+      content:
+        application/json:
+          example:
+            brand: BMW
+            model: X5
+    responses:
+      201:
+        description: Car created successfully
+    """
     content = request.get_json()
     car = Car.create_from_dto(content)
     car_controller.create(car)
@@ -21,6 +50,23 @@ def create_car() -> Response:
 
 @car_bp.get('/<int:car_id>')
 def get_car(car_id: int) -> Response:
+    """
+    Get car by ID
+    ---
+    tags:
+      - Cars
+    parameters:
+      - name: car_id
+        in: path
+        required: true
+        schema:
+          type: integer
+    responses:
+      200:
+        description: Car found
+      404:
+        description: Car not found
+    """
     car = car_controller.find_by_id(car_id)
     if car:
         return make_response(jsonify(car.put_into_dto()), HTTPStatus.OK)
@@ -28,6 +74,28 @@ def get_car(car_id: int) -> Response:
 
 @car_bp.put('/<int:car_id>')
 def update_car(car_id: int) -> Response:
+    """
+    Update car by ID
+    ---
+    tags:
+      - Cars
+    parameters:
+      - name: car_id
+        in: path
+        required: true
+        schema:
+          type: integer
+    requestBody:
+      required: true
+      content:
+        application/json:
+          example:
+            brand: Audi
+            model: A6
+    responses:
+      200:
+        description: Car updated
+    """
     content = request.get_json()
     car = Car.create_from_dto(content)
     car_controller.update(car_id, car)
@@ -35,5 +103,20 @@ def update_car(car_id: int) -> Response:
 
 @car_bp.delete('/<int:car_id>')
 def delete_car(car_id: int) -> Response:
+    """
+    Delete car by ID
+    ---
+    tags:
+      - Cars
+    parameters:
+      - name: car_id
+        in: path
+        required: true
+        schema:
+          type: integer
+    responses:
+      204:
+        description: Car deleted
+    """
     car_controller.delete(car_id)
     return make_response("Car deleted", HTTPStatus.NO_CONTENT)
--- my_project/auth/route/orders/ModelBlueprint.py
+++ my_project/auth/route/orders/ModelBlueprint.py
@@ -1,19 +1,48 @@
 from http import HTTPStatus
 from flask import Blueprint, jsonify, Response, request, make_response
 from my_project.auth.controller import model_controller
-
 from my_project.auth.domain.orders.Model import Model
 
 model_bp = Blueprint('model', __name__, url_prefix='/model')
 
 @model_bp.get('')
 def get_all_models() -> Response:
+    """
+    Get all models
+    ---
+    tags:
+      - Models
+    responses:
+      200:
+        description: List of models
+        examples:
+          application/json: [
+            {"id": 1, "brand_id": 2, "name": "X5"},
+            {"id": 2, "brand_id": 3, "name": "A6"}
+          ]
+    """
     models = model_controller.find_all()
     models_dto = [model.put_into_dto() for model in models]
     return make_response(jsonify(models_dto), HTTPStatus.OK)
 
 @model_bp.post('')
 def create_model() -> Response:
+    """
+    Create a new model
+    ---
+    tags:
+      - Models
+    requestBody:
+      required: true
+      content:
+        application/json:
+          example:
+            brand_id: 2
+            name: "X5"
+    responses:
+      201:
+        description: Model created successfully
+    """
     content = request.get_json()
     model = Model.create_from_dto(content)
     model_controller.create(model)
@@ -21,6 +50,23 @@ def create_model() -> Response:
 
 @model_bp.get('/<int:model_id>')
 def get_model(model_id: int) -> Response:
+    """
+    Get model by ID
+    ---
+    tags:
+      - Models
+    parameters:
+      - name: model_id
+        in: path
+        required: true
+        schema:
+          type: integer
+    responses:
+      200:
+        description: Model found
+      404:
+        description: Model not found
+    """
     model = model_controller.find_by_id(model_id)
     if model:
         return make_response(jsonify(model.put_into_dto()), HTTPStatus.OK)
@@ -28,6 +74,28 @@ def get_model(model_id: int) -> Response:
 
 @model_bp.put('/<int:model_id>')
 def update_model(model_id: int) -> Response:
+    """
+    Update model by ID
+    ---
+    tags:
+      - Models
+    parameters:
+      - name: model_id
+        in: path
+        required: true
+        schema:
+          type: integer
+    requestBody:
+      required: true
+      content:
+        application/json:
+          example:
+            brand_id: 3
+            name: "A6"
+    responses:
+      200:
+        description: Model updated
+    """
     content = request.get_json()
     model = Model.create_from_dto(content)
     model_controller.update(model_id, model)
@@ -35,5 +103,20 @@ def update_model(model_id: int) -> Response:
 
 @model_bp.delete('/<int:model_id>')
 def delete_model(model_id: int) -> Response:
+    """
+    Delete model by ID
+    ---
+    tags:
+      - Models
+    parameters:
+      - name: model_id
+        in: path
+        required: true
+        schema:
+          type: integer
+    responses:
+      204:
+        description: Model deleted
+    """
     model_controller.delete(model_id)
     return make_response("Model deleted", HTTPStatus.NO_CONTENT)
